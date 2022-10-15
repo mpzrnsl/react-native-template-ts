2362f706cd8e13e83d2958be2dffc641f4e970d0
Phia Nasol <mpzrnsl@outlook.com>
2022-10-16 05:34:24 +0800

    Initialize project.

---
+++ script.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+console.log("Running post init script for react-native-template-ts");
+
+console.log("Completed post init script for react-native-template-ts");
---
+++ template/src/App.tsx
@@ -0,0 +1,21 @@
+/**
+ * ProjectName
+ *
+ * Author:
+ * Company:
+ *
+ *
+ */
+
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>ProjectName</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
