1505e542a11b4629b687cc1da8c040627d7edc1e
Phia Nasol <mpzrnsl@outlook.com>
2022-10-16 22:19:36 +0800

    Setup environment config for Android.

--- template/android/app/proguard-rules.pro
+++ template/android/app/proguard-rules.pro
@@ -8,3 +8,7 @@
 #   http://developer.android.com/guide/developing/tools/proguard.html
 
 # Add any project specific keep options here:
+
+# react-native-config
+-keep class com.projectname.BuildConfig { *; }
+-keepresources string/build_config_package
--- template/src/App.tsx
+++ template/src/App.tsx
@@ -10,10 +10,13 @@
 import React from "react";
 import { SafeAreaView, Text } from "react-native";
 
+import Env from "react-native-config";
+
 const App = () => {
   return (
     <SafeAreaView>
       <Text>ProjectName</Text>
+      <Text>{Env.ENV}</Text>
     </SafeAreaView>
   );
 };
