ba2868ad1a8cd6a3d48ff8d673f53f4a8c921d60
Phia Nasol <mpzrnsl@outlook.com>
2022-10-17 06:04:27 +0800

    Add error boundary.

--- template/src/App.tsx
+++ template/src/App.tsx
@@ -11,25 +11,34 @@ import React from "react";
 import { SafeAreaView, Text } from "react-native";
 
 import Env from "react-native-config";
+import ErrorBoundary from "react-native-error-boundary";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 
-import { persistor, store } from "@core";
+import { Logger, persistor, store } from "@core";
 
 import { useLifecycleLogger } from "@core/hooks";
+
+const appErrorHandler = (error: Error, stackTrace: string) => {
+  const logger = Logger.extend("SYSTEM");
+
+  logger.error(error, stackTrace);
+};
 
 const App = () => {
   useLifecycleLogger(App.name);
 
   return (
-    <Provider store={store}>
-      <PersistGate persistor={persistor}>
-        <SafeAreaView>
-          <Text>ProjectName</Text>
-          <Text>{Env.ENV}</Text>
-        </SafeAreaView>
-      </PersistGate>
-    </Provider>
+    <ErrorBoundary onError={appErrorHandler}>
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <SafeAreaView>
+            <Text>ProjectName</Text>
+            <Text>{Env.ENV}</Text>
+          </SafeAreaView>
+        </PersistGate>
+      </Provider>
+    </ErrorBoundary>
   );
 };
 
