8eecb1022e0a2b9f4b202966adbfb5c395e63c0b
Phia Nasol <mpzrnsl@outlook.com>
2022-10-18 05:38:45 +0800

    Setup UI kit for Android.

--- template/src/App.tsx
+++ template/src/App.tsx
@@ -9,17 +9,20 @@
 import "react-native-gesture-handler";
 
 import React from "react";
-import { SafeAreaView, Text } from "react-native";
+import { SafeAreaView, Text, useColorScheme } from "react-native";
 
 import { NavigationContainer } from "@react-navigation/native";
+import { ThemeProvider } from "@rneui/themed";
 import Env from "react-native-config";
 import ErrorBoundary from "react-native-error-boundary";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 
 import { Logger, persistor, store } from "@core";
 
 import { useLifecycleLogger } from "@core/hooks";
+import { darkNavigationTheme, lightNavigationTheme, theme } from "@core/styles";
 
 const appErrorHandler = (error: Error, stackTrace: string) => {
   const logger = Logger.extend("SYSTEM");
@@ -30,21 +33,25 @@ const appErrorHandler = (error: Error, stackTrace: string) => {
 const App = () => {
   useLifecycleLogger(App.name);
 
+  const isDarkMode = useColorScheme() === "dark";
+
   return (
-    <React.StrictMode>
-      <ErrorBoundary onError={appErrorHandler}>
-        <Provider store={store}>
-          <PersistGate persistor={persistor}>
-            <NavigationContainer>
-              <SafeAreaView>
-                <Text>ProjectName</Text>
-                <Text>{Env.ENV}</Text>
-              </SafeAreaView>
+    <ErrorBoundary onError={appErrorHandler}>
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <SafeAreaProvider>
+            <NavigationContainer theme={isDarkMode ? darkNavigationTheme : lightNavigationTheme}>
+              <ThemeProvider theme={theme}>
+                <SafeAreaView>
+                  <Text>ProjectName</Text>
+                  <Text>{Env.ENV}</Text>
+                </SafeAreaView>
+              </ThemeProvider>
             </NavigationContainer>
-          </PersistGate>
-        </Provider>
-      </ErrorBoundary>
-    </React.StrictMode>
+          </SafeAreaProvider>
+        </PersistGate>
+      </Provider>
+    </ErrorBoundary>
   );
 };
 
